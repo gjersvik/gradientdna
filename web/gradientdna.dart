bb1ceec6c417f005cb762ff2c1924a695cc3de21
Ole Martin Gjersvik <olem@olem.org>
2014-03-11 11:23:18 +0100

    It now paint dna.

--- web/gradientdna.dart
+++ web/gradientdna.dart
@@ -7,13 +7,31 @@ part 'src/color.dart';
 part 'src/ddna.dart';
 
 void main() {
-  var ddna = new Ddna();
+  var ddna = new Ddna.random();
   
   CanvasElement canvas = querySelector('#gradient');
   CanvasRenderingContext2D paint = canvas.getContext('2d');
   
-  paint.fillStyle = ddna.bgColor().toString();
+  paintDdna(paint, ddna);
+}
+
+paintDdna(paint, ddna){
+  paint.fillStyle = ddna.bgColor.toString();
   paint.fillRect(0,0,256,256);
   
-  print(ddna.ddna.length);
+  ddna.gradiants.forEach((Gradiant g){
+    CanvasGradient gradient;
+    if(g.radial){
+      gradient = paint.createRadialGradient(g.x0, g.y0, g.r0, g.x1, g.y1, g.r1);
+    }else{
+      gradient = paint.createLinearGradient(g.x0, g.y0, g.x1, g.y1);
+    }
+    
+    g.steps.forEach((Step s){
+      gradient.addColorStop(s.offset, s.color.toString());
+    });
+    
+    paint.fillStyle = gradient;
+    paint.fillRect(0,0,256,256);
+  });
 }
--- web/src/ddna.dart
+++ web/src/ddna.dart
@@ -2,13 +2,57 @@ part of gradientdna;
 
 class Ddna{
   List<double> ddna;
+  Color bgColor;
+  List<Gradiant> gradiants;
   
-  Ddna(){
-    var random = new Random();
-    ddna = new List.generate(3,(_) => random.nextDouble());
+  Ddna(this.ddna){
+    bgColor = new Color.ddna(ddna.take(3));
+    gradiants = new List.generate(10, (i){
+          var skip = 3 + i * 58;
+          return new Gradiant(ddna.skip(skip).take(58));
+        });
   }
   
-  Color bgColor(){
-    return new Color.ddna(ddna.take(3));
+  factory Ddna.random(){
+    var random = new Random();
+    return new Ddna(new List.generate(583,(_) => random.nextDouble()));
+  }
+}
+
+class Step{
+  double offset;
+  Color color;
+  
+  Step(Iterable<double> ddna){
+    offset = ddna.first;
+    color = new Color.ddna(ddna.skip(1));
+  }
+}
+
+class Gradiant{
+  double x0;
+  double x1;
+  double y0;
+  double y1;
+  double r0;
+  double r1;
+  bool radial;
+  
+  List<Step> steps;
+  
+  Gradiant(Iterable<double> ddna){
+    var local = ddna.take(8);
+    x0 = ddna.elementAt(0) * 256;
+    x1 = ddna.elementAt(1) * 256;
+    y0 = ddna.elementAt(2) * 256;
+    y1 = ddna.elementAt(3) * 256;
+    r0 = ddna.elementAt(4) * 256;
+    r1 = ddna.elementAt(5) * 256;
+    radial = ddna.elementAt(6) >= 0.5;
+    var stepcount = (ddna.elementAt(7) * 9).floor() + 2;
+    steps = new List.generate(stepcount, (i){
+      var skip = 8 + i * 5;
+      return new Step(ddna.skip(skip).take(5));
+    });
   }
 }
