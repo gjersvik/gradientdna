e6c66f1f5f58fdbc7eb084eaba30a98f183f70cb
Ole Martin Gjersvik <olem@olem.org>
2014-03-15 12:04:32 +0100

    New mutate

--- web/src/mutate.dart
+++ web/src/mutate.dart
@@ -3,9 +3,18 @@ part of gradientdna;
 class Mutate{
   var random = new Random();
   
-  
   List<Ddna> populate(int childrencunt, Ddna select()){
-    return new List.generate(childrencunt,(_) => mate(select(), select()));
+    List<Ddna> list = new List.generate((childrencunt ~/ 5) + 1, (_) => select());
+    for(int i = 0; i < childrencunt ~/ 5; i += 1){
+      list.addAll(mate2(list[i], list[i + 1]));
+    }
+    return list;
+  }
+  
+  Ddna mutate(Ddna d){
+    var point = random.nextInt(583);
+    d.ddna[point] = random.nextDouble();
+    return d;
   }
   
   Ddna mate(Ddna a, Ddna b){
@@ -13,6 +22,31 @@ class Mutate{
     var list = [];
     list.addAll(a.ddna.take(cross));
     list.addAll(b.ddna.skip(cross));
-    return new Ddna(list);
+    return mutate(new Ddna(list));
+  }
+  
+  List<Ddna> mate2(Ddna a, Ddna b){
+    var list = [];
+    var cross = random.nextInt(583);
+    list.add(new Ddna(new List()
+      ..addAll(a.ddna.take(cross))
+      ..addAll(b.ddna.skip(cross))
+    ));
+    list.add(new Ddna(new List()
+      ..addAll(b.ddna.take(cross))
+      ..addAll(a.ddna.skip(cross))
+    ));
+    
+    cross = random.nextInt(583);
+    list.add(new Ddna(new List()
+      ..addAll(a.ddna.take(cross))
+      ..addAll(b.ddna.skip(cross))
+    ));
+    list.add(new Ddna(new List()
+      ..addAll(b.ddna.take(cross))
+      ..addAll(a.ddna.skip(cross))
+    ));
+    
+    return list.map(mutate).toList();
   }
 }
